cb0c0a204ce6fb20ef1fcc131672c4e8cd18ef9d
pb-gr <78931834+pb-gr@users.noreply.github.com>
2021-02-20 22:54:43 +0000

    Fixed minor issue
    Hopefully fixed actions for good

--- tools/data_builder.py
+++ tools/data_builder.py
@@ -264,7 +264,7 @@ def process_md_texts(md_texts):
 updated_at = datetime.now(timezone.utc).isoformat()
 
 md_header = f"""
-GENERATED FILE, PLEASE MAKE EDITS ON MASTER AT https://github.com/source_link = f"https://github.com/pb-gr/police-brutality-greece/blob/main/reports/{prefecture}.md"/
+GENERATED FILE, PLEASE MAKE EDITS ON MASTER AT https://github.com/source_link = f"https://github.com/pb-gr/police-brutality-greece/blob/main/incidents/{prefecture}.md"/
 UPDATED AT: {updated_at}
 """
 
